b44bede29838b2c93e4e7640c3465827dbba4d79
thomasNotTomas <thomasnottomas@gmail.com>
2024-05-25 19:42:21 -0300

    controls on video

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,15 @@ function App() {
           <div className="flex flex-col w-full items-center justify-center lg:flex-row">
           <img src="/workingCat.png" alt="" className="hidden 2xl:block" />
           <div className=" w-full flex justify-center">
-            <Windows95 title={'BUY WORKING CAT!!'} subtitle={'0x2cF14b7d4a77646F7Bb91EAe0f7C2d295c769fd3'}></Windows95>
+            <div className="w-96 windows-95">
+              <p className="windows-95-title">A cute fluffy guy that’s always working. Such a workaholic, he won’t stop working! Always on his laptop, doing important biz… Work work work, he can’t think about anything else.</p>
+            </div>
           </div>
           <img src="/workingCat.png" alt="" className="transform -scale-x-100" />
           </div>
         </section>
-        <section className="flex w-full max-w-[1500px] h-fit p-8 justify-between">
-        <div className="hidden 2xl:block">
+        <section className="flex flex-col xl:flex-row  w-full max-w-[1500px] h-fit p-8 justify-center space-x-8 w-full items-center space-y-8">
+        <div className="hidden xl:block">
         <Windows95 title={'BUY WORKING CAT!!'} subtitle={'0x2cF14b7d4a77646F7Bb91EAe0f7C2d295c769fd3'}></Windows95>
         </div>
         <VideoPlayer></VideoPlayer>
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,27 +1,19 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 
 function VideoPlayer() {
-    const [isMuted, setIsMuted] = useState(false);
-    const videoRef = useRef(null);
 
-    const toggleMute = () => {
-        setIsMuted(!isMuted);
-        videoRef.current.muted = !isMuted;
-    };
 
     return(
 
         <div>
             <video
-            ref={videoRef}
+
             src="/workingcat.MP4"
             autoPlay
             loop
-            muted={isMuted}
+            controls
             style={{width: '400px', height: 'auto'}}></video>
-            <button onClick={toggleMute}>
-                {isMuted ? 'Unmute' : 'Mute'}
-            </button>
+
         </div>
         
     );
