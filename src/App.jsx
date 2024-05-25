bfbcd259b4635245cd9b07a6fc7597f7431ada17
thomasNotTomas <thomasnottomas@gmail.com>
2024-05-25 11:23:49 -0300

    font fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
           <div className=" w-full flex items-center justify-center">
             <div className="windows-95 w-fit h-fit">
               <div className="windows-95-title flex min-w-20">
-              <h1 className="">Working Cat</h1>
+              <h1 className="">BUY WORKING CAT!!</h1>
               </div>
               <h2 className="mt-2">0x2cF14b7d4a77646F7Bb91EAe0f7C2d295c769fd3</h2>
               <div className="windows-95-button mt-2">COPY!</div>
