27f71ae6d7bf997125ae27589afafb7fe4a638e1
thomasNotTomas <thomasnottomas@gmail.com>
2024-05-24 21:34:01 -0300

    second cat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
-      <div>
+      <div className="flex flex-col items-center
+                      bg-[url('/background.jpg')] h-[1000px]">
         <img src="/logo.png" alt="" />
+        <section>
+          <img src="/workingCat.png" alt="" />
+        </section>
       </div>
     </>
   )
