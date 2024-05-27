1c38004093716607468011896d89e0140753dc65
thomasNotTomas <thomasnottomas@gmail.com>
2024-05-27 18:17:47 -0300

    ca fixed

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,10 @@ function App() {
           </a>
         </section>
         <img src="/logo.png" alt="" />
-        <h1 className="hidden md:block windows-95 text-2xl font-bold w-fit">CA: 0x2cF14b7d4a77646F7Bb91EAe0f7C2d295c769fd3</h1>
+        <div className="flex windows-95">
+        <h1 className="hidden md:block  text-2xl font-bold w-fit">CA:   </h1>
+        <h1 className="hidden md:block  text-2xl font-bold w-fit">0x2cF14b7d4a77646F7Bb91EAe0f7C2d295c769fd3</h1>
+        </div>
         <section className="flex w-full max-w-[1500px]">
           <div className="flex flex-col w-full items-center justify-center lg:flex-row">
           <img src="/workingCat.png" alt="" className="hidden 2xl:block" />
@@ -44,31 +47,30 @@ function App() {
         </section>
         <Slider></Slider>
         <section className="place-items-center grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 ">
-          <img className="w-80 h-auto mb-4 lg:mr-4" src="/memes/apple.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/bankchina.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/beach.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/bed.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/catnip.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/chinese.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/cornerwcat.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/crook.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/diamond.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/ezmoney.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/greener.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/haha1.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/japan.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/kfc.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/lambo.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/maga.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/minecraft.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/musk1.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/musk2.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/petrochina.jpg" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/pumpit.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/raiden.png" alt="" />
-          <img className="w-80 h-auto mb-4" src="/memes/untzcat.png" alt="" />
-          
-          <img className="w-80 h-auto mb-4" src="/memes/raydenwcat.png" alt="" />
+          <img className="w-[400px] h-auto mb-4 lg:mr-4" src="/memes/apple.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/bankchina.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/beach.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/bed.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/catnip.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/chinese.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/cornerwcat.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/crook.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/ezmoney.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/greener.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/haha1.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/japan.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/kfc.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/lambo.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/maga.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/minecraft.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/musk1.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/musk2.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/petrochina.jpg" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/pumpit.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/raiden.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/untzcat.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/campbell.png" alt="" />
+          <img className="w-[400px] h-auto mb-4" src="/memes/raydenwcat.png" alt="" />
         </section>
         <Slider></Slider>
         <section className="w-full flex justify-center items-center">
